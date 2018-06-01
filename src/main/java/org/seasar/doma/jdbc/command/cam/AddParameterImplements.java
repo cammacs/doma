ddccce414a7d4e7279c027de68c693171ec9efb9
kshiroma <shiroma@cam-net.co.jp>
2018-06-01 18:27:12 +0900

    機能拡張
    - コンパイルエラーになるので調整を入れる

--- src/main/java/org/seasar/doma/jdbc/command/cam/AddParameterImplements.java
+++ src/main/java/org/seasar/doma/jdbc/command/cam/AddParameterImplements.java
@@ -7,7 +7,8 @@ package org.seasar.doma.jdbc.command.cam;
 import org.seasar.doma.internal.expr.Value;
 
 public interface AddParameterImplements {
-    public void addParameter(String name, Class<?> type, Object value);
+    public default void addParameter(String name, Class<?> type, Object value) {
+    }
 
     public default void addParameter(String name, Value value) {
         addParameter(name, value.getType(), value.getValue());
